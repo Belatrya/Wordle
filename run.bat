d00305d9f273e0008b9cab7f1422b1736fb4d515
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-05-26 11:06:34 +0300

    removed extra comments, added gitignore rules for idea and compilation files

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
-rem chcp 65001
 javac -d out/production/Wordle -sourcepath src src/base/Game.java
 java -cp out/production/Wordle base.Game
 pause 
--- src/base/Game.java
+++ src/base/Game.java
@@ -2,6 +2,5 @@ package base;
 
 public class Game {
     public static void main(String[] args) {
-        System.out.println();
     }
 }
