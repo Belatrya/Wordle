66ea3769ab1f6252ee3d61f49d9b8cba08e45ed3
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-05-27 14:14:06 +0300

    removed logic from the Checker constructor into the separated method check

--- src/main/java/checkers/Checker.java
+++ src/main/java/checkers/Checker.java
@@ -5,15 +5,23 @@ import main.java.base.Word;
 public class Checker {
     private Word word;
 
-    public Checker(String word, int ruleWordLength) {
-        if (word.length() == ruleWordLength) {
-            this.word = new Word(word);
-        } else {
-            throw new IllegalArgumentException();
-        }
+    public Checker(String word) {
+        this.word = new Word(word);
     }
 
     public Word getWord() {
         return word;
+    }
+
+    public boolean isExistingWord() {
+
+        return true;
+    }
+
+    public boolean isLettersCountCorrect(int ruleWordLength) {
+        if (word.getLettersCount() == ruleWordLength) {
+            return true;
+        }
+        return false;
     }
 }
