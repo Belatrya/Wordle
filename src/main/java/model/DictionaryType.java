22e157c0103177d530504b8cdffc4c665257f53d
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-06-09 13:39:25 +0300

    fixed path to be more os independent

--- src/main/java/model/DictionaryType.java
+++ src/main/java/model/DictionaryType.java
@@ -1,15 +1,16 @@
 package model;
 
+import static java.io.File.separatorChar;
+
 /**
  * Describes the types of used dictionary with the path where to find them.
  */
 public enum DictionaryType {
-    HIDDEN_WORDS(".\\out\\resources\\hiddenWords.txt"),
-    ALL_WORDS(".\\out\\resources\\russian5letterwords.txt");
-
+    HIDDEN_WORDS(String.format(".%sout%sresources%shiddenWords.txt", separatorChar, separatorChar, separatorChar)),
+    ALL_WORDS(String.format(".%sout%sresources%srussian5letterwords.txt", separatorChar, separatorChar, separatorChar));
     private String path;
 
-    DictionaryType (String path){
+    DictionaryType(String path) {
         this.path = path;
     }
 
