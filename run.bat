b3222867db9f5f4738a589dd1670228851873830
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-07-11 12:30:36 +0300

    Added spring core libraries and fixed the bat file accordingly

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-javac -d .\out\production\Wordle -sourcepath src\main\java src\main\java\Main.java
+javac -d .\out\production\Wordle -sourcepath src\main\java -classpath src\main\resources\spring\spring-context-5.2.9.RELEASE.jar;src\main\resources\spring\spring-beans-5.2.9.RELEASE.jar;src\main\resources\spring\spring-core-5.2.9.RELEASE.jar;src\main\resources\spring\spring-aop-5.2.9.RELEASE.jar;src\main\resources\spring\spring-expression-5.2.9.RELEASE.jar;src\main\resources\spring\spring-jcl-5.2.9.RELEASE.jar; src\main\java\Main.java
 
 IF NOT EXIST .\out\resources (
 	md .\out\resources
