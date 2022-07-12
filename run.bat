76984bc4078945d1e280b94c8b29768006a0850b
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-07-12 11:21:03 +0300

    fixed the bat file with a cleaning the production directory before the compilation removed some spring dependencies until it is not nessesary

--- run.bat
+++ run.bat
@@ -1,9 +1,15 @@
 @echo off
-javac -d .\out\production\Wordle -sourcepath src\main\java -classpath src\main\resources\spring\spring-context-5.2.9.RELEASE.jar;src\main\resources\spring\spring-beans-5.2.9.RELEASE.jar;src\main\resources\spring\spring-core-5.2.9.RELEASE.jar;src\main\resources\spring\spring-aop-5.2.9.RELEASE.jar;src\main\resources\spring\spring-expression-5.2.9.RELEASE.jar;src\main\resources\spring\spring-jcl-5.2.9.RELEASE.jar; src\main\java\Main.java
+
+IF EXIST .\out\production (
+    del  .\out\production /q
+    )
+
+javac -d .\out\production\Wordle -sourcepath src\main\java -classpath src\main\resources\spring\* src\main\java\Main.java
 
 IF NOT EXIST .\out\resources (
 	md .\out\resources
-	)
+    )
+
 copy .\src\main\resources\dictionary\russian5letterwords.txt .\out\resources >NUL
 copy .\src\main\resources\dictionary\hiddenWords.txt .\out\resources >NUL
 
