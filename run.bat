24425fdd70014fff5f5dabd19a147536adb46b3f
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-07-14 12:10:16 +0300

    changed copy to robocopy utility

--- run.bat
+++ run.bat
@@ -6,14 +6,7 @@ IF EXIST .\out\production (
 
 javac -d .\out\production\Wordle -sourcepath src\main\java -classpath src\main\resources\spring\* src\main\java\Main.java
 
-IF NOT EXIST .\out\resources (
-	md .\out\resources\dictionary
-	md .\out\resources\spring
-    )
-
-copy .\src\main\resources\dictionary .\out\resources\dictionary >NUL
-copy .\src\main\resources\spring .\out\resources\spring >NUL
-copy .\src\main\resources\application.properties .\out\resources\application.properties >NUL
+robocopy .\src\main\resources .\out\resources  /E >Nul
 
 java -cp .\out\production\Wordle;.\out\resources\spring\*;.\out\resources Main
 pause
