dfe7bcafb18c239027722b46fa0ca427463b9b04
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-07-14 11:45:46 +0300

    moved application.properties to resources dorectory, fixed bat file with copy resources

--- run.bat
+++ run.bat
@@ -7,11 +7,13 @@ IF EXIST .\out\production (
 javac -d .\out\production\Wordle -sourcepath src\main\java -classpath src\main\resources\spring\* src\main\java\Main.java
 
 IF NOT EXIST .\out\resources (
-	md .\out\resources
+	md .\out\resources\dictionary
+	md .\out\resources\spring
     )
 
-copy .\src\main\resources\dictionary\russian5letterwords.txt .\out\resources >NUL
-copy .\src\main\resources\dictionary\hiddenWords.txt .\out\resources >NUL
+copy .\src\main\resources\dictionary .\out\resources\dictionary >NUL
+copy .\src\main\resources\spring .\out\resources\spring >NUL
+copy .\src\main\resources\application.properties .\out\resources\application.properties >NUL
 
-java -cp .\out\production\Wordle;src\main\resources\spring\* Main
+java -cp .\out\production\Wordle;.\out\resources\spring\*;.\out\resources Main
 pause
