f607e7dce671166e13f65867e224dd7d5691eb83
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-07-27 23:40:02 +0300

    updated bat file for starting tomcat server

--- run.bat
+++ run.bat
@@ -1,7 +1,18 @@
 @echo off
 
-call mvn clean package >nul
+set CATALINA_HOME=%1
 
-java -jar target\wordle-0.1.0.jar
+if exist "%CATALINA_HOME%\bin\catalina.bat" (
+	call mvn clean package >nul
+
+	copy .\target\wordle-0.1.0.war %CATALINA_HOME%\webapps >Nul
+	
+	cd /d %CATALINA_HOME%\bin\
+	
+	call catalina.bat run
+	
+) else (
+	echo The Tomcat directory is not defined correctly, please specify it as the first parameter for the "run.bat"
+)
 
 pause
