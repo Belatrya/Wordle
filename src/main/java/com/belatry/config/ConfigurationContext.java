8843d1b6244aacbaa10b5188d1f8c091b5aeae1f
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-08-02 17:52:33 +0300

    added open api support

--- src/main/java/com/belatry/config/ConfigurationContext.java
+++ src/main/java/com/belatry/config/ConfigurationContext.java
@@ -1,14 +1,11 @@
 package com.belatry.config;
 
-import lombok.AllArgsConstructor;
 import com.belatry.model.Dictionary;
 import com.belatry.model.DictionaryFileStorage;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,6 +23,14 @@ import java.nio.charset.StandardCharsets;
 @PropertySource("classpath:application.properties")
 @EnableWebMvc
 @AllArgsConstructor
+@Import({
+        org.springdoc.webmvc.ui.SwaggerConfig.class,
+        org.springdoc.core.SwaggerUiConfigProperties.class, org.springdoc.core.SwaggerUiOAuthProperties.class,
+        org.springdoc.webmvc.core.SpringDocWebMvcConfiguration.class,
+        org.springdoc.webmvc.core.MultipleOpenApiSupportConfiguration.class,
+        org.springdoc.core.SpringDocConfiguration.class, org.springdoc.core.SpringDocConfigProperties.class,
+        org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class
+})
 public class ConfigurationContext implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
