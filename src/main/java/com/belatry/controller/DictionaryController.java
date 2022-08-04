70b8ad570979a5ebb73cdf1b1d4ee2592a9f1993
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-08-05 02:38:06 +0300

    removed the error handling due to spring default

--- src/main/java/com/belatry/controller/DictionaryController.java
+++ src/main/java/com/belatry/controller/DictionaryController.java
@@ -1,7 +1,6 @@
 package com.belatry.controller;
 
 import com.belatry.model.Dictionary;
-import com.belatry.model.exceptions.DictionaryIsNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,7 +8,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,79 +33,41 @@ public class DictionaryController {
     }
 
     @Operation(summary = "Returns all words from the dictionary")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "all words from the dictionary",
-                    content = @Content(schema = @Schema(implementation = List.class))),
-            @ApiResponse(responseCode = "204", description = "The dictionary is not found!",
-                    content = @Content(schema = @Schema(implementation = String.class)))})
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "all words from the dictionary",
+            content = @Content(schema = @Schema(implementation = List.class)))})
     @GetMapping("/allWords")
     public ResponseEntity<?> showDictionary() {
-        try {
-            return ResponseEntity.ok(dictionary.getAllWords());
-        } catch (DictionaryIsNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NO_CONTENT);
-        }
+        return ResponseEntity.ok(dictionary.getAllWords());
     }
 
     @Operation(summary = "The word from the line by number")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "the word from the line by number"),
-            @ApiResponse(responseCode = "404", description = "The dictionary is not found!")})
     @GetMapping(value = "/{lineNumber}",
             produces = "text/plain;charset=UTF-8")
     public ResponseEntity<String> getWordFromLine(@PathVariable int lineNumber) {
-        try {
-            return ResponseEntity.ok(dictionary.getWord(lineNumber).get());
-        } catch (DictionaryIsNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
+        return ResponseEntity.ok(dictionary.getWord(lineNumber).get());
     }
 
     @Operation(summary = "Adds the word to the end of the file if the word doesn't exist yet")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "the word was added"),
-            @ApiResponse(responseCode = "404", description = "The dictionary is not found!")})
     @PostMapping("/add")
-    public ResponseEntity<?> addWord(@RequestParam String word) {
-        try {
-            dictionary.add(word);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (DictionaryIsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public void addWord(@RequestParam String word) {
+        dictionary.add(word);
     }
 
     @Operation(summary = "Deletes the word from the dictionary if it exists")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "the word was deleted"),
-            @ApiResponse(responseCode = "404", description = "The dictionary is not found!")})
     @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteWord(@RequestParam String word) {
-        try {
-            dictionary.delete(word);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (DictionaryIsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public void deleteWord(@RequestParam String word) {
+        dictionary.delete(word);
     }
 
     @Operation(summary = "Deleted the old word and added the new one instead of it")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "the word was edited"),
-            @ApiResponse(responseCode = "404", description = "The dictionary is not found!")})
     @PutMapping("/{oldValueWord}/edit")
-    public ResponseEntity<?> editWord(
+    public void editWord(
             @Parameter(description = "the old word value, should exist in the dictionary to be edited")
             @PathVariable String oldValueWord,
             @Parameter(description = "the new value for the word") @RequestParam String newValueWord) {
-        try {
-            if (dictionary.isExists(oldValueWord)) {
-                dictionary.add(newValueWord);
-                dictionary.delete(oldValueWord);
-            }
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (DictionaryIsNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (dictionary.isExists(oldValueWord)) {
+            dictionary.add(newValueWord);
+            dictionary.delete(oldValueWord);
         }
     }
 }
