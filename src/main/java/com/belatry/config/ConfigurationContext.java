d603680c6a325c6cbf142f497130ef519c7e6e19
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-08-18 18:39:05 +0300

    removed thymeleaf dependency

--- src/main/java/com/belatry/config/ConfigurationContext.java
+++ src/main/java/com/belatry/config/ConfigurationContext.java
@@ -11,13 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-
-import java.nio.charset.StandardCharsets;
 
 /**
  * Represents spring configuration.
@@ -48,31 +42,5 @@ public class ConfigurationContext implements WebMvcConfigurer {
     @Value("${hidden.words.path}")
     Dictionary hiddenWordsDictionary(String path) {
         return new DictionaryFileStorage(path);
-    }
-
-    @Bean
-    SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(applicationContext);
-        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
-        return templateResolver;
-    }
-
-    @Bean
-    SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        templateEngine.setEnableSpringELCompiler(true);
-        return templateEngine;
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        registry.viewResolver(resolver);
     }
 }
