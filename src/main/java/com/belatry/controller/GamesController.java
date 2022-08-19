7a75c9679d05df9dae5834a387117c7380af4965
BELATRY\belatry <natalya.stankovam@gmail.com>
2022-08-19 16:46:40 +0300

    added api docs for the games controller

--- src/main/java/com/belatry/controller/GamesController.java
+++ src/main/java/com/belatry/controller/GamesController.java
@@ -3,6 +3,9 @@ package com.belatry.controller;
 import com.belatry.model.Game;
 import com.belatry.model.UserGames;
 import com.belatry.model.exceptions.GameIsNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +23,17 @@ public class GamesController {
     private Game game;
     private UserGames userGames;
 
+    @Operation(summary = "Starts new game and save it in the storage")
+    @ApiResponses({@ApiResponse(responseCode = "201", description = "the game is created")})
     @PostMapping("/startnew")
     public ResponseEntity<?> startNewGame(HttpSession session) {
         userGames.addUserGame(session.getId(), game.createHiddenWord());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Returns the hidden word for the current user's game")
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "the hidden word"),
+            @ApiResponse(responseCode = "404", description = "the game was not found")})
     @GetMapping(value = "/game/hiddenWord", produces = "text/plain;charset=UTF-8")
     public ResponseEntity<String> getHiddenWord(HttpSession session) {
         try {
@@ -35,6 +43,9 @@ public class GamesController {
         }
     }
 
+    @Operation(summary = "Returns the status for the current user's game")
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "the game status"),
+            @ApiResponse(responseCode = "404", description = "the game was not found")})
     @GetMapping(value = "/game/state")
     public ResponseEntity<?> getGameState(HttpSession session) {
         try {
@@ -44,6 +55,9 @@ public class GamesController {
         }
     }
 
+    @Operation(summary = "Returns the round number for the current user's game")
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "the game round number"),
+            @ApiResponse(responseCode = "404", description = "the game was not found")})
     @GetMapping(value = "/game/currentRound")
     public ResponseEntity<?> getGameRoundNumber(HttpSession session) {
         try {
